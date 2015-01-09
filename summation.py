be23f6f4bdf58c973777e151dae452842f2844cd
vforgione <the.vincedent@icloud.com>
2015-01-09 16:30:46 -0600

    rm seq nums

--- summation.py
+++ summation.py
@@ -30,7 +30,8 @@ def sum_clicks(client, series):
 
 def write_sums(client, results, write_time, series):
     scrambled_write_points = []
-    write_columns = ['time', 'sequence_number', 'clicks', 'content_id']
+    # write_columns = ['time', 'sequence_number', 'clicks', 'content_id']
+    write_columns = ['time', 'clicks', 'content_id']
 
     for result in results:
         columns = result['columns']
@@ -39,7 +40,7 @@ def write_sums(client, results, write_time, series):
         for point in points:
             d = dict(zip(columns, point))
             d['time'] = write_time
-            d['sequence_number'] = 1
+            # d['sequence_number'] = 1
             scrambled_write_points.append(d)
 
     write_points = []
@@ -49,10 +50,21 @@ def write_sums(client, results, write_time, series):
             point.append(d[key])
         write_points.append(point)
 
+    # write_points.sort(key=lambda p: p[2], reverse=True)
+    write_points.sort(key=lambda p: p[1], reverse=True)
+    # for point in write_points[:100]:
+    #     body = [{
+    #         'name': series,
+    #         'columns': write_columns,
+    #         'points': [point, ],
+    #     }]
+    #     print(body)
+    # client.write_points(body)
+
     body = [{
         'name': series,
         'columns': write_columns,
-        'points': write_points,
+        'points': write_points[:100],
     }]
     print(body)
     client.write_points(body)
