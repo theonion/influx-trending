9740112d0850c4a210164a11e16cebed942d6d26
vforgione <the.vincedent@icloud.com>
2015-01-12 13:08:24 -0600

    fixing bugs

--- popular.py
+++ popular.py
@@ -1,77 +1,42 @@
 #!/usr/bin/env python3
 
-"""
-doing stuff i really wish we could have done natively in influxdb -- what a disappointment
-"""
-
-from datetime import datetime
-import logging
-import time
-
 from influxdb import InfluxDBClient
 
 
-def get_thresholds(client, percentile_series, time_offset):
-    """gets timestamp and threshold tuples from a given series within a given offset
+def main(client, percentile_series, content_series, popular_series, offset):
 
-    :param client: an influxdb client
-    :param percentile_series: the name of series containing the percentile values
-    :param time_offset: the influxdb formatted time offset
-    :return: a list of timestamp, threshold tuples
-    """
-    query = 'select * from {}'.format(percentile_series)
-    results = client.query(query)
-
-    if len(results):
-        results = results[0]
+    threshold_query = 'select * ' \
+                      'from {} ' \
+                      'where time > now() - {}'.format(percentile_series, offset)
+    threshold_results = client.query(threshold_query)
+    if len(threshold_results):
+        threshold_results = threshold_results[0]
     else:
-        n = datetime.now()
-        now = time.mktime(datetime(n.year, n.month, n.day, n.hour, 0, 0, 0).timetuple())
-        results = {'points': [[now, None, 1], ]}
+        return None
 
-    points = results.get('points', [])
-    return [(timestamp, threshold) for timestamp, _, threshold in points[:2]]
+    for th_timestamp, _, threshold in threshold_results.get('points', []):
+        print('threshold: {}, timestamp: {}'.format(threshold, th_timestamp))
+        content_query = 'select * ' \
+                        'from {} ' \
+                        'where clicks >= {} ' \
+                        'and time > now() - {}'.format(content_series, threshold, offset)
+        content_results = client.query(content_query)
+        if len(content_results):
+            content_results = content_results[0]
+        else:
+            return None
 
-
-def get_content(client, content_series, time_offset, threshold):
-    """gets content points that match a given time offset and have click values within the threshold
-
-    :param client: an influxdb client
-    :param content_series: the name of the series containing the content
-    :param time_offset: the influxdb formatted time offset
-    :param threshold: the minimum number of clicks allowable
-    :return: a list of time, sequence_number, clicks, content_id lists
-    """
-    query = 'select * from {} where clicks >= {} and time > now() - {}'.format(content_series, threshold, time_offset)
-    results = client.query(query)
-
-    if len(results):
-        results = results[0]
-    else:
-        results = {}
-
-    points = results.get('points', [])
-    return points
-
-
-def write_popular_point(client, popular_series, content, threshold):
-    """writes trend values to a given series so that it can be later recalled or rendered
-
-    :param client: and influxdb client
-    :param popular_series: the name of the series to write to
-    :param content: the content/point lists from `get_content`
-    :param threshold: the minimum number of clicks allowable
-    """
-    body = [{
-        'name': popular_series,
-        'columns': ['time', 'sequence_number', 'clicks', 'content_id', 'threshold'],
-        'points': [[t[0], 1, t[2], t[3], threshold] for t in content],
-    }]
-    try:
+        body = [{
+            'name': popular_series,
+            'columns': ['time', 'clicks', 'content_id', 'threshold'],
+            'points': [
+                [p[0], p[2], p[3], threshold]
+                for p in content_results.get('points', [])
+                if p[0] == th_timestamp
+            ],
+        }]
+        print('{}'.format(body))
         client.write_points(body)
-        print(body)
-    except Exception as e:
-        logging.error('`write_trend_point` write operation failed: {}'.format(str(e)))
 
 
 if __name__ == '__main__':
@@ -82,16 +47,4 @@ if __name__ == '__main__':
     # create client
     client = InfluxDBClient(host, port, username, password, db)
 
-    # get thresholds; iterate
-    thresholds = get_thresholds(client, percentile_series, offset)
-    for timestamp, threshold in thresholds:
-        # log it
-        logging.info('getting content: >= {} on {}'.format(threshold, timestamp))
-
-        # get content
-        content = get_content(client, content_series, offset, threshold)
-        # log it
-        logging.info('found {} points that match'.format(len(content)))
-
-        # write to series
-        write_popular_point(client, popular_series, content, threshold)
+    main(client, percentile_series, content_series, popular_series, offset)
--- summation.py
+++ summation.py
@@ -6,42 +6,41 @@ import time
 from influxdb import InfluxDBClient
 
 
-def make_write_time(rounding):
+def main(client, read_series, write_series, write_rounding):
+
     now = datetime.now()
-
-    offset = int(rounding[:-1])
-
-    if rounding.endswith('m'):
+    offset = int(write_rounding[:-1])
+    if write_rounding.endswith('m'):
         discard = timedelta(minutes=now.minute % offset, seconds=now.second, microseconds=now.microsecond)
-    elif rounding.endswith('h'):
-        discard = timedelta(minutes=now.minute, seconds=now.second, microseconds=now.microsecond)
+    elif write_rounding.endswith('h'):
+        discard = timedelta(
+            hours=now.hour % offset, minutes=now.minute, seconds=now.second, microseconds=now.microsecond)
+    elif write_rounding.endswith('d'):
+        discard = timedelta(
+            days=now.day % offset, hours=now.hour, minutes=now.minute, seconds=now.second,
+            microseconds=now.microsecond)
     else:
-        raise Exception('rounding must be done to minutes or hours')
-
+        raise Exception('rounding must be done to minutes, hours or days')
     thyme = now - discard
-    return time.mktime(thyme.timetuple())
+    write_time = time.mktime(thyme.timetuple())
 
+    content_query = 'select sum(clicks) as clicks ' \
+                    'from {} ' \
+                    'group by content_id'.format(read_series)
+    results = client.query(content_query)
+    if len(results):
+        results = results[0]
+    else:
+        return None
 
-def sum_clicks(client, series):
-    query = 'select sum(clicks) as clicks from {} group by content_id'.format(series)
-    results = client.query(query)
-    return results
-
-
-def write_sums(client, results, write_time, series):
     scrambled_write_points = []
-    # write_columns = ['time', 'sequence_number', 'clicks', 'content_id']
     write_columns = ['time', 'clicks', 'content_id']
-
-    for result in results:
-        columns = result['columns']
-        points = result['points']
-
-        for point in points:
-            d = dict(zip(columns, point))
-            d['time'] = write_time
-            # d['sequence_number'] = 1
-            scrambled_write_points.append(d)
+    columns = results.get('columns', [])
+    points = results.get('points', [])
+    for point in points:
+        d = dict(zip(columns, point))
+        d['time'] = write_time
+        scrambled_write_points.append(d)
 
     write_points = []
     for d in scrambled_write_points:
@@ -50,19 +49,9 @@ def write_sums(client, results, write_time, series):
             point.append(d[key])
         write_points.append(point)
 
-    # write_points.sort(key=lambda p: p[2], reverse=True)
     write_points.sort(key=lambda p: p[1], reverse=True)
-    # for point in write_points[:100]:
-    #     body = [{
-    #         'name': series,
-    #         'columns': write_columns,
-    #         'points': [point, ],
-    #     }]
-    #     print(body)
-    # client.write_points(body)
-
     body = [{
-        'name': series,
+        'name': write_series,
         'columns': write_columns,
         'points': write_points[:100],
     }]
@@ -72,10 +61,10 @@ def write_sums(client, results, write_time, series):
 
 if __name__ == '__main__':
     import sys
+    # get values
     _, host, port, username, password, db, read_series, write_series, write_rounding = sys.argv
 
+    # make client
     client = InfluxDBClient(host, port, username, password, db)
 
-    write_time = make_write_time(write_rounding)
-    results = sum_clicks(client, read_series)
-    write_sums(client, results, write_time, write_series)
+    main(client, read_series, write_series, write_rounding)
