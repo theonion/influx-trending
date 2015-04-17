7e0f4c980ea0e8cf09014301f2e39040eaf54f98
vforgione <v.forgione@gmail.com>
2015-04-17 14:11:53 -0500

    removed scripts - they are in ansible role

--- scripts/daily.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for APP in avclub clickhole videohub onion;
-do
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/popular.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1d.percentile.99" "$APP.rollup.1d" "$APP.rollup.1d.popular.99" "2d" && deactivate
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/summation.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1d" "$APP.rollup.1d.sums" "1d" && deactivate
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/trending.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1d.sums" "$APP.rollup.1d.trending" "3d" && deactivate
-done
-exit 0
--- scripts/hourly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for APP in avclub clickhole videohub onion;
-do
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/popular.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1h.percentile.99" "$APP.rollup.1h" "$APP.rollup.1h.popular.99" "2h" && deactivate
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/summation.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1h" "$APP.rollup.1h.sums" "1h" && deactivate
-    source /var/venvs/influx-trending/bin/activate && python /var/influx-trending/operations/trending.py "97.107.142.62" "8086" "root" "oaWUAkUdUu" "influxdb" "$APP.rollup.1h.sums" "$APP.rollup.1h.trending" "3h" && deactivate
-done
-exit 0
